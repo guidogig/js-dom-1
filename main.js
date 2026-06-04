2808b65e19c67d62a187eb3ef774b4c584e2d2cc
Guido Giglioli <guido.tobugroup@gmail.com>
2026-06-04 09:18:17 +0200

    rinominata variabile bg per leggibilita

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let accesa = false;
 const lightbulb = document.getElementById("lightbulb");
 const switchBtn = document.getElementById("switchBtn");
-const bgCol = document.getElementById("container");
+const bg = document.getElementById("container");
 
 const changeStatus = () => {
     if (accesa == false) {
-        bgCol.classList.add("light");
-        bgCol.classList.remove("dark");
+        bg.classList.add("light");
+        bg.classList.remove("dark");
         lightbulb.src = "img/yellow_lamp.png";
         switchBtn.innerText = "Spegni";
         accesa = true;
         console.log("Ho acceso la lampadina");
     } else {
-        bgCol.classList.add("dark");
-        bgCol.classList.remove("light");
+        bg.classList.add("dark");
+        bg.classList.remove("light");
         lightbulb.src = "img/white_lamp.png";
         switchBtn.innerText = "Accendi";
         accesa = false;
